3da7e5c4f3500b4581e93f3312fbc0af7322b1a3
deep-sarkar <deepsarkar96@gmail.com>
2020-02-13 14:45:35 +0530

    [Deep] added sumOfThree function

--- FunctionalProg/functional.py
+++ FunctionalProg/functional.py
@@ -11,3 +11,19 @@ class Functional:
         for row_index in array:
             print(row_index)
         return array
+
+#Sum of 3 digit in array equal to zero
+    def sumOfThree(self):
+        num_of_ele = int(input('Enter number of elements : '))
+        array = [0 for index in range(num_of_ele)]
+        for index in range(num_of_ele):
+            array[index] = int(input('Enter element : '))
+        print(array)
+        for i in range(num_of_ele):
+            j = i+1
+            for j in range(j,num_of_ele):
+                k = j+1
+                for k in range(k,num_of_ele):
+                    if array[i] + array[j] + array[k] == 0 :
+                        print(array[i] , array[j], array[k])
+                
