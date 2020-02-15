4178dc36cfe1a496ed253c3e659752bdc95d3b8e
deep-sarkar <deepsarkar96@gmail.com>
2020-02-15 17:36:11 +0530

    [Deep] updated merge sort method

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -30,3 +30,50 @@ class AlgorithmMethods:
             for j in range(length):
                 if array[j] > array[i]:
                     array[i],array[j] = array[j],array[i]
+
+#Merge sort algorithm
+    def merge(self,arr, low, mid, high): 
+        left_length = mid - low + 1
+        right_length = high - mid 
+    
+        # create temporary arrays 
+        left_temp_array = [0 for i in range(left_length)] 
+        right_temp_array = [0 for i in range(right_length)] 
+    
+        #Copy elemnt to left and right temp array 
+        for i in range(0 , left_length): 
+            left_temp_array[i] = arr[low + i] 
+    
+        for j in range(0 , right_length): 
+            right_temp_array[j] = arr[mid + 1 + j] 
+     
+        i = 0     # Initial index of first subarray 
+        j = 0     # Initial index of second subarray 
+        k = low    # Initial index of merged subarray 
+
+        while i < left_length and j < right_length : 
+            if left_temp_array[i] < right_temp_array[j]: 
+                arr[k] = left_temp_array[i] 
+                i += 1
+            else: 
+                arr[k] = right_temp_array[j] 
+                j += 1
+            k += 1
+    
+        while i < left_length: 
+            arr[k] = left_temp_array[i] 
+            i += 1
+            k += 1
+
+        while j < right_length: 
+            arr[k] = right_temp_array[j] 
+            j += 1
+            k += 1
+     
+    # sub-array of arr to be sorted 
+    def mergeSort(self,arr,low,high): 
+        if low < high: 
+            mid = (low+(high-1))//2
+            self.mergeSort(arr, low, mid)       #Sort 1st half
+            self.mergeSort(arr, mid+1, high)    #Sort 2nd half
+            self.merge(arr, low, mid, high)     #merge
