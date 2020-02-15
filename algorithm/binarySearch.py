563006c24a49c33c1d69ce116fbc42c0979fb63f
deep-sarkar <deepsarkar96@gmail.com>
2020-02-15 14:55:38 +0530

    [Deep] modified binary search program

--- algorithm/binarySearch.py
+++ algorithm/binarySearch.py
@@ -2,6 +2,7 @@ from algorithm import *
 class BinarySearch:
     b = AlgorithmMethods()
     array = ['Apple','Boy','Cat','Dog','Elephant']
+    print(array)
     length = len(array)
     word = str(input('Enter word : '))
     index = b.binSearch(array, 0, length, word)
