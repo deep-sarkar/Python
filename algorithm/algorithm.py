bf7ac05ef8b120e80f24b308828df39cf63fa367
deep-sarkar <deepsarkar96@gmail.com>
2020-02-15 14:54:20 +0530

    [Deep] added insertion sort method

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -1,5 +1,6 @@
 class AlgorithmMethods:
 
+#Binary search method
     def binSearch(self,array,low,high,element):
         if low > high:
             return -1
@@ -10,4 +11,14 @@ class AlgorithmMethods:
             return mid
         elif array[mid] < element :
             return self.binSearch(array, mid + 1, high , element) 
-  
+
+#Insertion sort method
+    def insertion_sort(self,array):
+        length = len(array)
+        for i in range(1,length):
+            temp = array[i]
+            j = i-1
+            while j >= 0 and array[j] > temp:
+                array[j+1] = array[j]
+                j -= 1
+            array[j+1] = temp
