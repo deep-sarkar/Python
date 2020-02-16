b2a3a385fa6c2ac9ec55c33f404ac39e2439600c
deep-sarkar <deepsarkar96@gmail.com>
2020-02-16 17:23:04 +0530

    [Deep] removed unwanted files

--- algorithm/primeNumbers.py
+++
@@ -1,8 +0,0 @@
-from algorithm import *
-class PrimeNumber:
-    pr = AlgorithmMethods()
-    lower_range = int(input('Enter lower range : '))
-    upper_range = int(input('Enter lower range : '))
-    pr.prime_in_range(lower_range,upper_range)
-
-    
--- algorithm/stringPermutation.py
+++
@@ -1,7 +0,0 @@
-from algorithm import *
-class Permutation:
-    per = AlgorithmMethods()
-    string = str(input('Enter word : '))
-    length = len(string) 
-    arr = list(string)
-    per.permute(arr,0,length-1)
