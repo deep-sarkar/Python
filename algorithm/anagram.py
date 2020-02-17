8a6dbcbeecc4ef142eab6e2da9769faf2c306436
deep-sarkar <deepsarkar96@gmail.com>
2020-02-17 17:48:29 +0530

    [Deep]Removed.removed unwanted files

--- algorithm/anagram.py
+++
@@ -1,9 +0,0 @@
-from algorithm import *
-class AnagramDetection:
-    al = AlgorithmMethods()
-    string1 = input('Enter 1st line : ')
-    string2 = input('Enter 2nd line : ')
-    if al.is_anagram(string1,string2):
-        print('Anagram')
-    else:
-        print('Not an anagrams')
--- algorithm/mergeSort.py
+++
@@ -1,8 +0,0 @@
-from algorithm import *
-class MergeSort:
-    ins = AlgorithmMethods()
-    ar = ['Dog','Boy','Apple','Elephant','Cat']
-    n = len(ar)
-    print('Before sorting : ',ar)
-    ins.mergeSort(ar,0,n-1)
-    print('After sorting : ',ar)
