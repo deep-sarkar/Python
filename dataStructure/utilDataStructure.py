370292c91108bbcb2e3daeefc8efa69ffa789f49
deep-sarkar <deepsarkar96@gmail.com>
2020-02-20 15:32:39 +0530

    [Deep]Add.added method to generate 2d array of prime number

--- dataStructure/utilDataStructure.py
+++ dataStructure/utilDataStructure.py
@@ -50,3 +50,25 @@ class UtilDataStructure:
                     return False
                 j += 1
             return True
+    '''
+        - allPrime() method to print all prime number in fortm of 2d array
+    '''
+    def allPrime(self):
+        lower_range = 2
+        upper_range = 100
+        ar =[]
+        for i in range(10):
+            li = []
+            for j in range(lower_range,upper_range):
+                if len(li) == 0:
+                    li.append(lower_range)
+                    li.append(' - ')
+                    li.append(upper_range)
+                    li.append(' : ')
+                if self.is_prime(j):
+                    li.append(j)
+                if j == upper_range - 1:
+                    lower_range = upper_range
+                    upper_range += 100
+                    ar.append(li)
+        return ar
