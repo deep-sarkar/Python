e9578854c109996fa8fff2085963f282605c5e64
deep-sarkar <deepsarkar96@gmail.com>
2020-02-26 09:54:09 +0530

    [Deep]Add.added contact delete method

--- OOP/addressBook.py
+++ OOP/addressBook.py
@@ -1,12 +1,24 @@
 import json
 import re
 class AddressBook:
+    def __init__(self):
+        self.address_book = {"contact" : []}
         
     def addNew(self):
         flag = True
+        flag1 = True
         detail = {}
         detail['First_name'] = input('name : ')
         detail['Last_name'] = input('Last name : ')
+        try:
+            with open('address.json','r') as json_file:
+                data = json.load(json_file)
+            for person in data['contact']:
+                if detail['First_name'] == person['First_name'] and detail['Last_name'] == person['Last_name']:
+                    print('Contact already exist, please try again')
+                    return self.addNew()
+        except json.decoder.JSONDecodeError:
+            print('Adding 1st contact')
         while flag:
             mobile_number = input('mobile_number : ')
             regex = '[6789]{1}[0-9]{9}'
@@ -14,28 +26,57 @@ class AddressBook:
                 flag = False
                 detail['Mobile_no'] = int(mobile_number)
             else :
-                print('Invalid nmobile number, please enter again')
+                print('Invalid mobile number, please enter again')
         detail['state'] = input('state : ')
+        while flag1:
+            zip_code = input('ZIP code : ')
+            regex = '[0-9]{6}'
+            if re.search(regex,zip_code) :
+                flag1 = False
+                detail['zip_code'] = int(zip_code)
+            else :
+                print('Invalid zip code, please enter again')
         return detail
 
-a = AddressBook()
-address_book = {"contact" : []}
-
-while True:
-    exit = input('Do you want to add y/n ?')
-    if exit.lower() == 'n':
-        break
-    else:
+    def addToJson(self):
         try:
             with open('address.json','r') as f:
-                address_book = json.load(f)
+                self.address_book = json.load(f)
             detail = a.addNew()
-            address_book['contact'].append(detail)
+            self.address_book['contact'].append(detail)
         except json.decoder.JSONDecodeError:
             detail = a.addNew()
-            address_book['contact'].append(detail)
-    with open('address.json','w') as json_file:
-        json.dump(address_book,json_file,indent=2)
+            self.address_book['contact'].append(detail)
+        with open('address.json','w') as json_file:
+            json.dump(self.address_book,json_file,indent=2)
+       
+    def delete(self,name):
+        index = 0
+        try:
+            with open('address.json','r') as json_file:
+                self.address_book = json.load(json_file)
+            for element in self.address_book['contact']:
+                print(element['First_name'])
+                if element['First_name'] == name:
+                    print(element)
+                    self.address_book['contact'].pop(index)
+                index += 1
+        except json.decoder.JSONDecodeError:
+            print('Book is empty')
+        with open('address.json','w') as json_file:
+            json.dump(self.address_book,json_file,indent=2)
+
+
+a = AddressBook()
+while True:
+    print('\n1.add \n2.delete')
+    user_input = int(input('eneter your choice'))
+    if user_input == 1:
+        a.addToJson()
+    if user_input == 2:
+        name = input('Enter first name')
+        a.delete(name)
+
 
 # while True:
 #     exit = input('Do you want to see detaily y/n ?')
