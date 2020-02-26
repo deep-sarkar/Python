4313154f6d797816ca3cdf405db6b70cb30e1b58
deep-sarkar <deepsarkar96@gmail.com>
2020-02-26 17:22:52 +0530

    [Deep]Refactor.modified create new book method

--- OOP/addressBook/addressbookController.py
+++ OOP/addressBook/addressbookController.py
@@ -1,5 +1,17 @@
 from addressBook import *
+from fileSystem import *
+import fnmatch
 class userAddressBook:
+
+    print('-------------------------- My AddressBook ----------------------------')
+    all_books = fnmatch.filter(os.listdir('addressBook'), '*.json')
+    total_books = len(all_books)
+    print("total saved book : ",total_books)
+    book_count = 1
+    for book in all_books:
+        print(book_count,'.',book)
+        book_count += 1
+    print('------------------------------------------------------------------',end='')
     new_addressBook = AddressBook()
     while True:
         print('\n1.Open existing book \n2.Create new book \n3.Quit')
@@ -11,8 +23,7 @@ class userAddressBook:
         if user_input == 1:
             new_addressBook.open()
             while True:
-                print('''\n1.add \n2.delete \n3.display all name \n4.display detail \n5.edit \n6.Sort by Zip
-                \n7.Sort by name \n8.Display all contacts \n9.Quit''')
+                print('''\n1.add \n2.delete \n3.display all name \n4.display detail \n5.edit \n6.Sort by Zip \n7.Sort by name \n8.Display all contacts \n9.Quit''')
                 try:
                     user_input = int(input('eneter your choice : '))
                 except ValueError :
@@ -40,8 +51,15 @@ class userAddressBook:
                     new_addressBook.displayAll()
                 if user_input == 9:
                     break
+                print('***********************************************',end='')
         if user_input == 2:
-            new_addressBook.create()
+            new_file = FileSystem()
+            new_file.create()
+            my_file = input('To save file print y')
+            if my_file == 'y' or my_file == 'Y':
+                new_file.save()
+                print('File saved')
+        print('------------------------------------------------------------------',end='')
         if user_input == 3:
             break
 
