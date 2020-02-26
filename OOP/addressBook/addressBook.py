761d6f8c07be356001fcdc0be43b3d1387b015d1
deep-sarkar <deepsarkar96@gmail.com>
2020-02-26 17:22:02 +0530

    [Deep]Refactor.devided work load

--- OOP/addressBook/addressBook.py
+++ OOP/addressBook/addressBook.py
@@ -2,29 +2,13 @@ import json
 import re
 import operator
 import os.path
+import person
 
-class AddressBook:
+class AddressBook(person.Person):
     def __init__(self):
         self.address_book = {"contact" : []}
         self.path = 'addressBook/address.json'
 
-    '''
-        -create(self) when method will called, it will create a new file.
-    '''
-    def create(self):
-        try:
-            file_name = input('Enter file name : ')
-            file_name = file_name+'.json'
-            if os.path.isfile(file_name):
-                print ("File exist")
-                return self.create()
-            else :
-                file_name1 = 'addressBook/'+file_name
-                new_book = open(file_name1,'w+')
-                new_book.close()
-        except IOError:
-            print('File not found')
-    
     '''
         -open(self) method will open an existing file from directory
     '''
@@ -43,61 +27,6 @@ class AddressBook:
             print('New file opened')
             self.path = file_name1
             print(self.path)
-    
-    '''
-        -addNew(self) method woll collect all information from user and return it in 
-            the form of dictionary object
-    '''
-    def addNew(self):
-        flag = True
-        flag1 = True
-        detail = {}
-        detail['First_name'] = input('name : ')
-        detail['Last_name'] = input('Last name : ')
-        try:
-            with open(self.path,'r') as json_file:
-                data = json.load(json_file)
-            for person in data['contact']:
-                if detail['First_name'] == person['First_name'] and detail['Last_name'] == person['Last_name']:
-                    print('Contact already exist, please try again')
-                    return self.addNew()
-        except json.decoder.JSONDecodeError:
-            print('Adding 1st contact')
-        while flag:
-            mobile_number = input('mobile_number : ')
-            regex = '[6789]{1}[0-9]{9}'
-            if re.search(regex,mobile_number) :
-                flag = False
-                detail['Mobile_no'] = int(mobile_number)
-            else :
-                print('Invalid mobile number, please enter again')
-        detail['city'] = input('city : ')
-        detail['state'] = input('state : ')
-        while flag1:
-            zip_code = input('ZIP code : ')
-            regex = '[0-9]{6}'
-            if re.search(regex,zip_code) :
-                flag1 = False
-                detail['zip_code'] = int(zip_code)
-            else :
-                print('Invalid zip code, please enter again')
-        return detail
-
-    '''
-        -addToJson(self) method internally calling addNew(self) method and saving detail in opened
-            file
-    '''
-    def addToJson(self):
-        try:
-            with open(self.path,'r') as json_file:
-                self.address_book = json.load(json_file)
-            detail = self.addNew()
-            self.address_book['contact'].append(detail)
-        except json.decoder.JSONDecodeError:
-            detail = self.addNew()
-            self.address_book['contact'].append(detail)
-        with open(self.path,'w') as json_file:
-            json.dump(self.address_book,json_file,indent=2)
     
     '''
         -delete(self,first_name) method taking first_name and Last_name as an argument and will delete perticular name
