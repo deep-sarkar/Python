d54ca8d09e9deeb4143a21029caa2a38e165f280
deep-sarkar <deepsarkar96@gmail.com>
2020-02-26 15:00:27 +0530

    [Deep]Refactor.modified all method's call

--- OOP/addressBook/addressbookController.py
+++ OOP/addressBook/addressbookController.py
@@ -1,32 +1,44 @@
 from addressBook import *
 class userAddressBook:
     new_addressBook = AddressBook()
-    flag = True
-    while flag:
-        print('''\n1.add \n2.delete \n3.display all name \n4.display detail \n5.edit \n6.Sort by Zip
-        \n7.Sort by name \n8.Quit''')
+    while True:
+        print('\n1.Open existing book \n2.Create new book \n3.Quit')
         try:
-            user_input = int(input('eneter your choice : '))
-        except ValueError :
-            print('Try again')
-            flag = True
+            user_input = int(input('Enter your choice : '))
+        except ValueError:
+            print('Please enter right option')
+            continue
         if user_input == 1:
-            new_addressBook.addToJson()
+            new_addressBook.open()
+            while True:
+                print('''\n1.add \n2.delete \n3.display all name \n4.display detail \n5.edit \n6.Sort by Zip
+                \n7.Sort by name \n8.Quit''')
+                try:
+                    user_input = int(input('eneter your choice : '))
+                except ValueError :
+                    print('Try again')
+                    continue
+                if user_input == 1:
+                    new_addressBook.addToJson()
+                if user_input == 2:
+                    name = input('Enter first name : ')
+                    new_addressBook.delete(name)
+                if user_input == 3:
+                    new_addressBook.displayContactName()
+                if user_input == 4:
+                    name = input('Enter 1st name : ')
+                    new_addressBook.displayContactDetail(name)
+                if user_input == 5:
+                    name = input('Enter 1st name : ')
+                    new_addressBook.editContact(name)
+                if user_input == 6:
+                    new_addressBook.sortByZIP()
+                if user_input == 7:
+                    new_addressBook.sortByName()
+                if user_input == 8:
+                    break
         if user_input == 2:
-            name = input('Enter first name : ')
-            new_addressBook.delete(name)
+            new_addressBook.create()
         if user_input == 3:
-            new_addressBook.displayContactName()
-        if user_input == 4:
-            name = input('Enter 1st name : ')
-            new_addressBook.displayContactDetail(name)
-        if user_input == 5:
-            name = input('Enter 1st name : ')
-            new_addressBook.editContact(name)
-        if user_input == 6:
-            new_addressBook.sortByZIP()
-        if user_input == 7:
-            new_addressBook.sortByName()
-        if user_input == 8:
-            flag = False
+            break
 
