4d8a414e921b568db7ec2cad9b6e077b0ce7de18
deep-sarkar <deepsarkar96@gmail.com>
2020-02-27 19:34:48 +0530

    [Deep]Refactor.modified card list

--- OOP/card/deckOfCard.py
+++ OOP/card/deckOfCard.py
@@ -2,7 +2,7 @@ import random
 
 class Card:
     def __init__(self):
-        self.cards = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+        self.cards = ['2 ','3 ','4 ','5 ','6 ','7 ','8 ','9 ','10','J ','Q ','K ','A ']
         self.suites = ['♥','♠','♣','♦']
     
     '''
